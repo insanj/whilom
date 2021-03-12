d9940a2fcc20991683d4a447b414068e6fdde30c
Julian Weiss <insanj@gmail.com>
2021-03-12 01:59:24 -0500

    anim attempts, wonderful progress yay

--- whilom/AppDelegate.swift
+++ whilom/AppDelegate.swift
@@ -14,8 +14,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     // MARK: left click menu item
     let whilomStatusItem: NSStatusItem = {
         var statusItem = NSStatusItem()
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.title = "🪄"
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+      
+        let hattieOffImage = NSImage(named: NSImage.Name("hattie off"))
+        hattieOffImage?.isTemplate = false // in the future, this will be handled by the instance var instead
+      
+        statusItem.button?.imageScaling = .scaleProportionallyUpOrDown
+        statusItem.button?.imageHugsTitle = true
+//        statusItem.button?.alternateImage =
+        statusItem.button?.image = hattieOffImage
+
         return statusItem
     }()
     
@@ -36,7 +44,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }()
   
     // MARK: scripts, states
-    var isSleepEnabled = false
+    var isSleepEnabled = true
   
     let enableSleepScript: NSAppleScript? = {
         let myAppleScript = """
@@ -63,6 +71,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         return scriptObject
     }()
+  
+    // MARK: - anim props
+    let animDuration: CFTimeInterval = 0.15
+    let imageHattieOff: NSImage = {
+        let image = NSImage(named: NSImage.Name("hattie off"))!
+        image.isTemplate = false
+        return image
+    }()
+  
+    let imageHattieOn1: NSImage = {
+        let image = NSImage(named: NSImage.Name("hattie on 1"))!
+        image.isTemplate = false
+        return image
+    }()
+
+    let imageHattieOn2: NSImage = {
+        let image = NSImage(named: NSImage.Name("hattie on 2"))!
+        image.isTemplate = false
+        return image
+    }()
+
+    let imageHattieOn3: NSImage = {
+        let image = NSImage(named: NSImage.Name("hattie on 3"))!
+        image.isTemplate = false
+        return image
+    }()
+    
   
     // MARK: - runtime
     func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -120,33 +155,86 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     // MARK: execute scripts based on state
     @objc private func disableSleep() -> Bool {
-        var error: NSDictionary?
-        disableSleepScript?.executeAndReturnError(&error)
+//        var error: NSDictionary?
+//        disableSleepScript?.executeAndReturnError(&error)
+//
+//        if let error = error {
+//            let alert = NSAlert(error: NSError(domain: "com.insanj.whilom", code: 0, userInfo: [NSLocalizedDescriptionKey: error["NSAppleScriptErrorMessage"]!]))
+//            alert.runModal()
+//            return false
+//        }
       
-        if let error = error {
-            let alert = NSAlert(error: NSError(domain: "com.insanj.whilom", code: 0, userInfo: [NSLocalizedDescriptionKey: error["NSAppleScriptErrorMessage"]!]))
-            alert.runModal()
-            return false
-        }
-        
-        whilomStatusItem.button?.title = "😴"
+        performSleepAnimation(forwards: true)
         isSleepEnabled = false
         return true
     }
     
     @objc private func enableSleep() -> Bool {
-        var error: NSDictionary?
-        enableSleepScript?.executeAndReturnError(&error)
-    
-        if let error = error {
-            let alert = NSAlert(error: NSError(domain: "com.insanj.whilom", code: 0, userInfo: [NSLocalizedDescriptionKey: error["NSAppleScriptErrorMessage"]!]))
-            alert.runModal()
-            return false
-        }
-      
-        whilomStatusItem.button?.title = "🪄"
+//        var error: NSDictionary?
+//        enableSleepScript?.executeAndReturnError(&error)
+//
+//        if let error = error {
+//            let alert = NSAlert(error: NSError(domain: "com.insanj.whilom", code: 0, userInfo: [NSLocalizedDescriptionKey: error["NSAppleScriptErrorMessage"]!]))
+//            alert.runModal()
+//            return false
+//        }
+//
+        performSleepAnimation(forwards: false)
         isSleepEnabled = true
         return true
+    }
+  
+    // MARK: - animations
+    private func performSleepAnimation(forwards: Bool) {
+        NSAnimationContext.beginGrouping()
+        NSAnimationContext.current.duration = animDuration
+        whilomStatusItem.button?.image = forwards ? imageHattieOn1 : imageHattieOn2
+        NSAnimationContext.endGrouping()
+      
+        DispatchQueue.main.asyncAfter(deadline: .now() + (animDuration * 1)) { [unowned self] in
+            NSAnimationContext.beginGrouping()
+            NSAnimationContext.current.duration = self.animDuration
+            self.whilomStatusItem.button?.image = forwards ? imageHattieOn2 : imageHattieOn1
+            NSAnimationContext.endGrouping()
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + (animDuration * 2)) { [unowned self] in
+            NSAnimationContext.beginGrouping()
+            NSAnimationContext.current.duration = self.animDuration
+            self.whilomStatusItem.button?.image = forwards ? imageHattieOn3 : imageHattieOff
+            NSAnimationContext.endGrouping()
+        }
+      
+//        let crossFade1 = CABasicAnimation(keyPath: "image")
+//        crossFade1.duration = duration
+//        crossFade1.fromValue = whilomStatusItem.button?.image
+//        crossFade1.toValue = hattieOn1
+//
+//        let crossFade2 = CABasicAnimation(keyPath: "image")
+//        crossFade2.duration = duration
+//        crossFade2.fromValue = hattieOn1
+//        crossFade2.toValue = hattieOn2
+//
+//        let crossFade3 = CABasicAnimation(keyPath: "image")
+//        crossFade3.duration = duration
+//        crossFade3.fromValue = hattieOn2
+//        crossFade3.toValue = hattieOn3
+//
+//
+//
+//        whilomStatusItem.button?.layer?.add(crossFade1, forKey: "stepOne")
+
+
+    }
+  
+    private func performEnableSleepAnimation() {
+  //      let hattieOffImage = NSImage(named: NSImage.Name("hattie off"))
+  //      hattieOffImage?.isTemplate = true
+  //      statusItem.button?.imageScaling = .scaleProportionallyUpOrDown
+  //      statusItem.button?.imageHugsTitle = true
+  //  //        statusItem.button?.alternateImage =
+  //      statusItem.button?.image = hattieOffImage
+
     }
 }
 
